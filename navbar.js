606648ff4ed504cfead78cd03df4e6571431db0a
baspop01 <73678928+baspop01@users.noreply.github.com>
2021-10-28 02:32:12 +0700

    Delete navbar.js

--- navbar.js
+++
@@ -1,6 +0,0 @@
-OpenNav = () => {
-    $("#navbar").slideToggle('slow');
-}
-document.getElementById('open_btn').addEventListener("click", function() {
-    this.classList.toggle("is-active");
-}, false);
