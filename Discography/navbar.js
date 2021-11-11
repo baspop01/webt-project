b320857a84776e6070683f862cbf36638871fb0f
baspop01 <73678928+baspop01@users.noreply.github.com>
2021-11-12 04:43:50 +0700

    Add files via upload

--- Discography/navbar.js
+++ Discography/navbar.js
@@ -1,5 +1,13 @@
+let i = 0;
 OpenNav = () => {
     $("#navbar").slideToggle('slow');
+    if (i%2==0){
+        document.body.style.overflow="hidden";
+    }
+    else{
+        document.body.style.overflow="auto";
+    }
+    i++;
 }
 document.getElementById('open_btn').addEventListener("click", function() {
     this.classList.toggle("is-active");
